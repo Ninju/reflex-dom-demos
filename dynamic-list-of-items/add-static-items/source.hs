8cceff0aa85951c9dc847c9ec549688956d20ae9
Alex Watt <el.zingo@gmail.com>
2016-07-06 14:03:13 +0300

    Add Demo #6: dynamic-list-of-items/add-static-item

---
+++ dynamic-list-of-items/add-static-items/source.hs
@@ -0,0 +1,26 @@
+{-# LANGUAGE RecursiveDo, FlexibleContexts #-}
+import Reflex
+import Reflex.Dom
+import Control.Monad
+import qualified Data.Map as Map
+
+renderStaticListOfItems :: MonadWidget t m => [String] -> m ()
+renderStaticListOfItems items = el "ul" $ do
+  mapM_ (el "li" . text) items
+
+renderDynamicList items = do
+  dynamicOfWidgets <- mapDyn renderStaticListOfItems items
+  dyn dynamicOfWidgets
+
+renderAddNewTaskForm = el "div" $ do
+  elAttr "label" ("for" =: "task-name") $ text "Task name: "
+  tInput <- textInput (def & attributes .~ constDyn (Map.singleton "id" "task-name"))
+  clickEvent <- button "Add new task"
+  return $ tagDyn (_textInput_value tInput) clickEvent
+
+main = mainWidget $ do
+  rec renderDynamicList currentTasks
+      newTask <- renderAddNewTaskForm
+      currentTasks <- foldDyn (:) [] newTask
+
+  return ()
