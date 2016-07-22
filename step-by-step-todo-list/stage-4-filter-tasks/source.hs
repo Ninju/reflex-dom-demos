33546c18e170bc9d40af30a42f77a6971d60e289
Alex <el.zingo@gmail.com>
2016-07-22 16:54:10 +0300

    Add stage-4-filter-tasks to step-by-step-todo-list

---
+++ step-by-step-todo-list/stage-4-filter-tasks/source.hs
@@ -0,0 +1,85 @@
+{-# LANGUAGE RecursiveDo #-}
+{-# LANGUAGE TemplateHaskell #-}
+import           Control.Lens
+import           Control.Monad
+import qualified Data.Map as Map
+import           Reflex
+import           Reflex.Dom
+
+data Task = Task { _taskDescription :: String
+                 , _taskCompleted :: Bool
+                 }
+                 deriving Show
+
+data Filter = All | Active | Completed deriving (Show, Eq)
+
+data UserEvent a = Delete | Create a | Edit (a -> a)
+
+makeLenses ''Task
+
+applyUserOp :: UserEvent a -> Maybe a -> Maybe a
+applyUserOp Delete = const Nothing
+applyUserOp (Create a) = const (Just a)
+applyUserOp (Edit f) = fmap f
+
+newTask :: String -> Task
+newTask taskDesc = Task { _taskDescription = taskDesc, _taskCompleted = False }
+
+initialTasks :: Map.Map Int Task
+initialTasks = foldl (\curMap id -> Map.insert id (newTask $ "Task #" ++ show id) curMap) Map.empty [1..5]
+
+satisfiesFilter :: Filter -> Task -> Bool
+satisfiesFilter All       = const True
+satisfiesFilter Completed = view taskCompleted
+satisfiesFilter Active    = not . view taskCompleted
+
+main = mainWidget app
+
+updateWithMap = flip (Map.foldlWithKey applyUserOperation)
+                where
+                applyUserOperation accMap key op = Map.alter (applyUserOp op) key accMap
+
+app = do
+  rec filterChange <- renderFilters [All, Active, Completed]
+      userEvents   <- renderApp filteredTasks
+      newTaskEvent <- renderNewTaskForm
+      newTaskIds   <- mapDyn (+6) =<< count newTaskEvent
+      tasks        <- foldDyn updateWithMap
+                              initialTasks
+                              (appendEvents userEvents $ attachDynWith Map.singleton newTaskIds newTaskEvent)
+
+      activeFilterDyn <- holdDyn All filterChange
+      filteredTasks   <- combineDyn (Map.filter . satisfiesFilter) activeFilterDyn tasks
+
+  return ()
+
+renderApp :: (Ord k, MonadWidget t m) => Dynamic t (Map.Map k Task) -> m (Event t (Map.Map k (UserEvent Task)))
+renderApp dynTasks = do
+  el "h1" $ text "Edit tasks"
+  el "ul" $ do
+    listViewWithKey dynTasks $ \k task -> do
+      el "li" $ do
+        dynText =<< mapDyn (view taskDescription) task
+        checkboxChange <- checkboxView (constDyn mempty) =<< mapDyn (view taskCompleted) task
+        deleteEvent <- button "Delete"
+
+        let deleteEvents = fmap (const Delete) deleteEvent
+        let editEvents = fmap (Edit . set taskCompleted) checkboxChange
+
+        return $ leftmost [deleteEvents, editEvents]
+
+renderNewTaskForm = do
+  elAttr "label" ("for" =: "new-task-name") $ text "Task name: "
+  t <- textInput (def & attributes .~ constDyn (Map.singleton "id" "new-task-name"))
+  clickEvent <- button "Create task"
+  dynCreates <- mapDyn (Create . newTask) (_textInput_value t)
+  return $ tagDyn dynCreates clickEvent
+
+renderFilters :: MonadWidget t m => [Filter] -> m (Event t Filter)
+renderFilters filters =
+  el "ul" $ do
+    filterEvents <- forM filters $ \filter -> do
+      filterClick <- button (show filter)
+      return $ fmap (const filter) filterClick
+
+    return $ leftmost filterEvents
